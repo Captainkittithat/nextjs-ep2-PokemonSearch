09c7703a8e2984d715650a03282b14b9f7a87f26
Captainkittithat <163084007+Captainkittithat@users.noreply.github.com>
2025-09-18 11:30:22 +0700

    Update page.jsx

--- src/app/pokesearch/[pokeName]/page.jsx
+++ src/app/pokesearch/[pokeName]/page.jsx
@@ -1,68 +1,88 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'next/navigation'
-import Link from 'next/link'
-import Image from 'next/image'
+import React, { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 function PokeResult() {
+  const params = useParams();
 
-    const params = useParams();
+  const [pokeData, setPokeData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [pokeData, setPokeData] = useState([]);
-    const [loading, setLoading] = useState(false);
+  console.log(pokeData);
 
-    console.log(pokeData)
-
-    const fetchPokeData = async () => {
-        try {
-            setLoading(true);
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokeName}`)
-            const data = await response.json();
-            setPokeData(data);
-            setLoading(false);
-        } catch(error) {
-            console.log(error)
-        }
+  const fetchPokeData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${params.pokeName}`
+      );
+      const data = await response.json();
+      setPokeData(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchPokeData();
+  useEffect(() => {
+    fetchPokeData();
+  }, [params.pokeName]);
 
   return (
-    <div className='p-24'>
-        <Link href="/" className='bg-blue-500 text-whit p-3 rounded-md'>Go Back</Link>
-        <div className='flex justify-center items-center mt-10 text-center'>
-            <div className='shadow-md p-10 rounded-md'>
-                {loading ? (
-                    <p>Loading...</p>
-                ) : (
-                    <>
-                        <h3 className='text-3xl'>{pokeData.name}</h3>
-                        <Image src={pokeData.sprites?.other.home.front_default} width={300} height={300} alt={pokeData.name} />
-                        <div className='mt-5'>
-                            <p className='my-3'>Weight: {pokeData.weight}</p>
-                            <p className='my-3'>
-                                Abilities: {" "}
-                                {pokeData.abilities?.map((val => (
-                                    <span key={val.ability.name} className='bg-gray-500 text-white px-3 py-1 rounded-md'>{val.ability.name}</span>
-                                )),
-                                )}
-                            </p>
-                            <p className='my-3'>
-                                Type: {" "}
-                                {pokeData.types?.map((val => (
-                                    <span key={val.type.name} className='bg-gray-500 text-white px-3 py-1 rounded-md'>{val.type.name}</span>
-                                )),
-                                )}
-                            </p>
-                        </div>
-                    </>
-                )}    
-            </div>
+    <div className="p-24">
+      <Link href="/" className="bg-blue-500 text-white p-3 rounded-md">
+        Go Back
+      </Link>
+      <div className="flex justify-center items-center mt-10 text-center">
+        <div className="shadow-md p-10 rounded-md">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            pokeData && (
+              <>
+                <h3 className="text-3xl">{pokeData.name}</h3>
+                <Image
+                  src={pokeData.sprites?.other?.home?.front_default}
+                  width={300}
+                  height={300}
+                  alt={pokeData.name}
+                />
+                <div className="mt-5">
+                  <p className="my-3">Weight: {pokeData.weight}</p>
+                  <p className="my-3">
+                    Abilities:{" "}
+                    {pokeData.abilities?.map((val) => (
+                      <span
+                        key={val.ability.name}
+                        className="bg-gray-500 text-white px-3 py-1 rounded-md mr-2"
+                      >
+                        {val.ability.name}
+                      </span>
+                    ))}
+                  </p>
+                  <p className="my-3">
+                    Type:{" "}
+                    {pokeData.types?.map((val) => (
+                      <span
+                        key={val.type.name}
+                        className="bg-gray-500 text-white px-3 py-1 rounded-md mr-2"
+                      >
+                        {val.type.name}
+                      </span>
+                    ))}
+                  </p>
+                </div>
+              </>
+            )
+          )}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PokeResult
+export default PokeResult;
